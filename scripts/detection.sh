365608ff9455ac5ad95d5921f76eec19e59bbacb
FKhan706 <farah.khan@apmgroup.co.uk>
2025-11-24 14:31:23 +0000

    Update detection.sh

--- scripts/detection.sh
+++ scripts/detection.sh
@@ -1,16 +1,31 @@
 #!/bin/bash
-FONT_DIR="/Library/Fonts"
+FONT_DST="/Library/Fonts"
 
 FONT_FILES=(
 "Poppins-Black.ttf"
+"Poppins-BlackItalic.ttf"
 "Poppins-Bold.ttf"
-# add all 18 font names here
+"Poppins-BoldItalic.ttf"
+"Poppins-ExtraBold.ttf"
+"Poppins-ExtraBoldItalic.ttf"
+"Poppins-ExtraLight.ttf"
+"Poppins-ExtraLightItalic.ttf"
+"Poppins-Italic.ttf"
+"Poppins-Light.ttf"
+"Poppins-LightItalic.ttf"
+"Poppins-Medium.ttf"
+"Poppins-MediumItalic.ttf"
+"Poppins-Regular.ttf"
+"Poppins-SemiBold.ttf"
+"Poppins-SemiBoldItalic.ttf"
+"Poppins-Thin.ttf"
+"Poppins-ThinItalic.ttf"
 )
 
 for font in "${FONT_FILES[@]}"; do
-    if [ ! -f "$FONT_DIR/$font" ]; then
-        exit 1
+    if [ ! -f "$FONT_DST/$font" ]; then
+        exit 1   # Font missing
     fi
 done
 
-exit 0
+exit 0   # All fonts installed
