c36eae77614176325ed9027cda8b533bfa7035e7
FKhan706 <farah.khan@apmgroup.co.uk>
2025-11-24 14:34:58 +0000

    detection.sh

--- scripts/detection.sh
+++ scripts/detection.sh
@@ -5,27 +5,13 @@ FONT_FILES=(
 "Poppins-Black.ttf"
 "Poppins-BlackItalic.ttf"
 "Poppins-Bold.ttf"
-"Poppins-BoldItalic.ttf"
-"Poppins-ExtraBold.ttf"
-"Poppins-ExtraBoldItalic.ttf"
-"Poppins-ExtraLight.ttf"
-"Poppins-ExtraLightItalic.ttf"
-"Poppins-Italic.ttf"
-"Poppins-Light.ttf"
-"Poppins-LightItalic.ttf"
-"Poppins-Medium.ttf"
-"Poppins-MediumItalic.ttf"
-"Poppins-Regular.ttf"
-"Poppins-SemiBold.ttf"
-"Poppins-SemiBoldItalic.ttf"
-"Poppins-Thin.ttf"
-"Poppins-ThinItalic.ttf"
+# ... all 18 font names
 )
 
 for font in "${FONT_FILES[@]}"; do
     if [ ! -f "$FONT_DST/$font" ]; then
-        exit 1   # Font missing
+        exit 1   # font missing
     fi
 done
 
-exit 0   # All fonts installed
+exit 0   # all fonts installed
