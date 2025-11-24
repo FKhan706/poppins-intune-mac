af20f9744eb5a41993984dfe03b297143a02c7bc
FKhan706 <farah.khan@apmgroup.co.uk>
2025-11-24 14:34:37 +0000

    install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-FONT_SRC="$(dirname "$0")/../fonts"   # path to fonts folder relative to scripts folder
+FONT_SRC="$(dirname "$0")/../fonts"   # relative path from scripts folder
 FONT_DST="/Library/Fonts"
 
-# Copy all .ttf files to system fonts folder
 for font in "$FONT_SRC"/*.ttf; do
     cp "$font" "$FONT_DST/"
     chmod 644 "$FONT_DST/$(basename $font)"
